1b153bae5805c73d810d1e6621243dfcac778650
strongmhk <kim6562166086@gmail.com>
2024-05-23 18:13:51 +0900

    feat: ReviewImage 엔티티 생성 및 양방향 매핑

--- src/main/java/umc/spring/domain/ReviewImage.java
+++ src/main/java/umc/spring/domain/ReviewImage.java
@@ -4,28 +4,20 @@ import jakarta.persistence.*;
 import lombok.*;
 import umc.spring.domain.common.BaseEntity;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
-public class Review extends BaseEntity {
+public class ReviewImage extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String title;
-
-    private Float score;
+    private String imageUrl;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "member_id")
-    private Member member;
-
-    @OneToMany(mappedBy = "review", cascade = CascadeType.ALL)
-    private List<ReviewImage> reviewImageList = new ArrayList<>();
+    @JoinColumn(name = "review_id")
+    private Review review;
 }
